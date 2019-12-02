055ee95d9da84d17d5ee818e2739ea7a1f7d9026
loisemuthoni <loisemburu01@gmail.com>
2019-12-02 15:00:35 +0300

    corrected the form

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -15,17 +15,18 @@ export class QuoteComponent implements OnInit {
       }
     }
   }
+  
   quote: Quote[] = [
-    new Quote(1, 'Watch finding Nemo', 'Find an online version and watch merlin find his son',new Date(2020,3,14)),
-    new Quote(2,'Buy Cookies','I have to buy cookies for the parrot',new Date(2020,3,14)),
-    new Quote(3,'Get new Phone Case','Diana has her birthday coming up soon',new Date(2020,3,14)),
-    new Quote(4,'Get Dog Food','Pupper likes expensive snacks',new Date(2020,3,14)),
-    new Quote(5,'Solve math homework','Damn Math',new Date(2020,3,14)),
-    new Quote(6,'Plot my world domination plan','Cause I am an evil overlord',new Date(2020,3,14)),
+    new Quote(1, 'The way to get started is to quit talking and begin doing.', '',new Date(2020,3,14)),
+    new Quote(2,'If life were predictable it would cease to be life, and be without flavor','',new Date(2020,3,14)),
+    new Quote(3,'Whoever is happy will make others happy too.','',new Date(2020,3,14)),
+    new Quote(4,'You will face many defeats in life, but never let yourself be defeated.','',new Date(2020,3,14)),
+    new Quote(5,'Never let the fear of striking out keep you from playing the game.','',new Date(2020,3,14)),
   ];
-   toggleDetails(index){
+  toggleDetails(index){
     this.quote[index].showDescription = !this.quote[index].showDescription;
   }
+   
   completeQuote(isComplete, index){
     if (isComplete) {
       this.quote.splice(index,1);
