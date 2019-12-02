775a3f9c32953f4e1181a6f062a318d38e172907
loisemuthoni <loisemburu01@gmail.com>
2019-12-02 11:17:51 +0300

    modified complete button

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -17,7 +17,7 @@ export class QuoteComponent implements OnInit {
    toggleDetails(index){
     this.quote[index].showDescription = !this.quote[index].showDescription;
   }
-  completeGoal(isComplete, index){
+  completeQuote(isComplete, index){
     if (isComplete) {
       this.quote.splice(index,1);
     }
